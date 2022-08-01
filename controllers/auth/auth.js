f50a98574a6671ac06c336517818751eeeacac62
ashayc247 <ashay.t@chapter247.com>
2022-08-01 15:52:13 +0530

    feat: implemented check for user with this email already exists

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -1,4 +1,8 @@
+const mongodb = require('mongodb');
+
 const User = require('../../models/user');
+
+const { isValueExists } = require('../../utilities/collections');
 
 const authUtilities = require('./utilities');
 
@@ -13,6 +17,22 @@ exports.addUser = async (request, response, next) => {
     const email = body.email;
     const password = body.password;
     const user = new User(firstName, lastName, email, password,);
+
+    const users = await User.fetchAll();
+
+    const userAlreadyExists = isValueExists(users, 'email', email);
+
+    if (userAlreadyExists === true) {
+
+      return response.status(200).json({
+        status: 'error',
+        statuscode: 400,
+        error: {
+          message: 'User with this email already exists'
+        }
+      });
+
+    }
 
     const userId = await user.save();
 
